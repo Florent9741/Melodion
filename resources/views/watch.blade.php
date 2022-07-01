0dbe459d2076aaa84643935c43a583544131302d
Ruff50 <cbolac@gmail.com>
2022-07-01 10:40:43 +0400

    player arno

--- resources/views/watch.blade.php
+++ resources/views/watch.blade.php
@@ -18,8 +18,8 @@
                 </form>
 
                 <div class="card mb-6  ">
-                    <div class=" embed-responsive embed-responsive-16by9">
-                        {{-- <iframe src="https://www.youtube.com/embed/{{$singleVideo->items[0]->id}}" width="889" height="500" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe> --}}
+                    
+                   <div class="flex flex-col">   
                         <div id="monplayer" class="w-full">
                             <!-- 1. The <iframe> (and video player) will replace this <div> tag. -->
                             <div class="flex flex-col">
@@ -321,51 +321,40 @@
                             <h5>{{ $singleVideo->items[0]->snippet->title }}</h5>
                             <p class="text-secondary">Published at
                                 {{ date('d M Y', strtotime($singleVideo->items[0]->snippet->publishedAt)) }}</p>
-                            <p>{{ $singleVideo->items[0]->snippet->description }}</p>
+                            <p>{{ \Illuminate\Support\Str::limit($singleVideo->items[0]->snippet->description,$limit=150,$end=' ...') }}</p>
                         </div>
 
-                <div class="card mb-6" style="width: 90%">
-                    <div class="embed-responsive embed-responsive-16by9">
-                        <iframe src="https://www.youtube.com/embed/{{$singleVideo->items[0]->id}}" width="600" height="500" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+               
+                    </div>  
+                </div>
+            </div>
+            <div class="w-2/5">
+                <div
+                    class="lg:flex-grow md:w-full lg:pl-24 md:pl-16 flex flex-col md:items-start md:text-left items-center text-center">
+                    <h1 class="title-font sm:text-4xl text-3xl mb-4 font-medium text-gray-900"> Rédiger un mémo
+                    </h1>
+                    <div class="relative mb-4">
+                        <label for="message" class="leading-7 text-sm text-gray-600">Message</label>
+                        <form action="" method="post">
+
+                            <input type="hidden" name="videoId" value="">
+
+                            <input type="hidden" name="user_id" value="">
+
+                            <textarea id="message" name="message" cols="50" rows="15"
+                                class="w-full bg-white rounded border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 h-64 text-base outline-none text-gray-700 py-1 px-3 resize-none leading-6 transition-colors duration-200 ease-in-out"></textarea>
+
+
+                        </form>
                     </div>
-                    <div class="card-body">
-                        <h5>{{$singleVideo->items[0]->snippet->title}}</h5>
-                        <p class="text-secondary">Published at {{date('d M Y', strtotime($singleVideo->items[0]->snippet->publishedAt))}}</p>
-                        <p>{{$singleVideo->items[0]->snippet->description}}</p>
+
+                    <div class="flex justify-center">
+                        <button
+                            class="inline-flex text-white bg-red-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded text-lg">
+                            Enregistrer </button>
 
                     </div>
                 </div>
             </div>
-                <div class="w-2/5">
-                    <div
-                        class="lg:flex-grow md:w-full lg:pl-24 md:pl-16 flex flex-col md:items-start md:text-left items-center text-center">
-                        <h1 class="title-font sm:text-4xl text-3xl mb-4 font-medium text-gray-900"> Rédiger un mémo
-                        </h1>
-                        <div class="relative mb-4">
-                            <label for="message" class="leading-7 text-sm text-gray-600">Message</label>
-                            <form action="" method="post">
-
-                                <input type="hidden" name="videoId" value="">
-
-                                <input type="hidden" name="user_id" value="">
-
-                                <textarea id="message" name="message" cols="50" rows="15"
-                                    class="w-full bg-white rounded border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 h-64 text-base outline-none text-gray-700 py-1 px-3 resize-none leading-6 transition-colors duration-200 ease-in-out"></textarea>
-
-
-                            </form>
-                        </div>
-
-                        <div class="flex justify-center">
-                            <button
-                                class="inline-flex text-white bg-red-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded text-lg">
-                                Enregistrer </button>
-
-                        </div>
-                    </div>
-                </div>
-
-            </div>
-        </div>
     </div>
 @endsection
