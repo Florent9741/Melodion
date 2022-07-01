e3539a87b4698f652e0f3a9172a2075d32ffae78
heylowdy <100339824+heylowdy@users.noreply.github.com>
2022-07-01 10:41:07 +0400

    commit

--- resources/views/watch.blade.php
+++ resources/views/watch.blade.php
@@ -5,7 +5,7 @@
     <div class="container mt-4">
         <div class="row">
             <div class="col-8">
-                <form action="{{ route('library') }}" method="post">
+                <form action="{{ route('watch') }}" method="post">
                     @csrf
                     <input type="hidden" name="videoId" value="{{$singleVideo->items[0]->id}}">
                     @if (null!==(Auth::user()))
