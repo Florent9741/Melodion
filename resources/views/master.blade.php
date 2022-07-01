fd2cb00f7f30ea448d5f98f70b7bc626cc575e30
Ruff50 <cbolac@gmail.com>
2022-07-01 15:11:24 +0400

    melodion sans l'accent

--- resources/views/master.blade.php
+++ resources/views/master.blade.php
@@ -30,7 +30,7 @@
                                   <div class="collapse navbar-collapse" id="navbarSupportedContent">
                                     <ul class="navbar-nav me-auto mb-2 mb-lg-0">
                                       <li class="nav-item">
-                                        <a class="nav-link active" aria-current="page" href="{{route('index')}}">Mélodion</a>
+                                        <a class="nav-link active" aria-current="page" href="{{route('index')}}">Melodion</a>
                                       </li>
                                       
                                     </ul>
