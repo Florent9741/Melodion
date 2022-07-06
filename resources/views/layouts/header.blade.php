60bff0936e5ab56991d07359e4dbb83879d852b9
arnowaro <arnowaro@live.fr>
2022-07-06 12:21:40 +0400

    ajout romain elodie

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -13,7 +13,7 @@
 
 
 
-                        {{-- Les boutons à droite  --}}
+                        Les boutons à droite
                         <div class="flex flex-row items-center m-0">
 
                             {{-- Affiche les boutons "connexion" et "inscription" si l'utilisateur n'est pas connecté --}}
@@ -41,6 +41,46 @@
                                     class=" block text-md px-4   py-2 rounded text-white font-bold hover:text-white  hover:bg-blue-700 lg:mt-0">Deconnexion</a>
                             @endauth
                         </div>
+
+                        <div class="flex justify-end fa-2xl mr-5">
+                            <i class="fa-solid fa-circle-user text-red-500 "></i>
+                            <button id="dropdownDefault" data-dropdown-toggle="dropdown" class=" bg-white focus:outline-none font-medium rounded-lg text-sm px-4 py-2.5 text-center inline-flex items-center " type="button"><i class="fa-solid fa-ellipsis-vertical fa-xl hover:text-red-500"></i><button>
+                            <!-- Dropdown menu -->
+                            <div id="dropdown" class="z-10 hidden bg-white divide-y divide-gray-100 rounded shadow w-44 ">
+                                <ul class="py-1 text-sm font-semibold text-black  " aria-labelledby="dropdownDefault">
+                                    @guest
+
+                                    <li>
+                                        <a href="{{route('index')}}" class="block px-4 py-2  hover:text-red-500">Connexion</a>
+                                    </li>
+
+                                    <li>
+                                        <a href="{{route('index')}}" class="block px-4 py-2  hover:text-red-500">Inscription</a>
+                                    </li>
+                                    @endguest
+                                  <li>
+                                    <a href="{{route('index')}}" class="block px-4 py-2  hover:text-red-500">Accueil</a>
+                                  </li>
+                                  @auth
+
+                                  <li>
+                                      <a href="{{route('biblio', Auth::user()->id)}}" class="block px-4 py-2 hover:text-red-500">Bibliothèque</a>
+                                    </li>
+
+                                    <li>
+                                        <a href="{{route('user')}}" class="block px-4 py-2 hover:text-red-500">Dashboard</a>
+                                    </li>
+
+                                    <li>
+                                        <a href="{{route('signout')}}" class="block px-4 py-2 hover:text-red-500">Sign Out</a>
+                                    </li>
+                                    @endauth
+                                </ul>
+                            </div>
+                            </div>
+                            </div>
+                            <script src="https://unpkg.com/flowbite@1.4.7/dist/flowbite.js"></script>
+
               </div>
          </div>
 
