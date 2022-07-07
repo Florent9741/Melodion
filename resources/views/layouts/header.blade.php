e2be9d2d86b344e440a9792215f60f07828b3398
arnowaro <arnowaro@live.fr>
2022-07-07 05:04:18 +0400

    mobile avec barre de recherche dans header

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -3,59 +3,66 @@
 </script>
 
 
-    <nav class="flex flex-row justify-between items-center flex-wrap bg-black py-2  lg:px-12 shadow ">
+<nav class="flex flex-row justify-between items-center flex-wrap bg-black py-2  lg:px-12 shadow ">
 
 
-                        {{-- titre Melodion cliquable à gauche, permet le retour vers page d'acceuil --}}
-                        <div class="">
-                            <a href="/" <span class="font-semibold text-white ml-2 ">Melodion</span> </a>
-                        </div>
+    {{-- titre Melodion cliquable à gauche, permet le retour vers page d'acceuil --}}
+    <div class="md:order-1">
+        <a href="/" <span class="font-semibold text-1xl text-white ml-2 ">Melodion</span> </a>
+    </div>
 
 
 
 
+    <div class="  md:order-3 flex justify-end gap-1 fa-2xl mr-5">
+        <i class="fa-solid fa-circle-user text-red-500 "></i>
+        <button id="dropdownDefault" data-dropdown-toggle="dropdown"
+            class=" bg-white focus:outline-none font-medium rounded-lg text-sm px-4 py-2.5 text-center inline-flex items-center "
+            type="button"><i class="fa-solid fa-ellipsis-vertical fa-xl hover:text-red-500"></i><button>
+                <!-- Dropdown menu -->
+                <div id="dropdown" class="z-10 hidden bg-white divide-y divide-gray-100 rounded shadow w-44 ">
+                    <ul class="py-1 text-sm font-semibold text-black  " aria-labelledby="dropdownDefault">
+                        @guest
 
-                        <div class="flex justify-end gap-1 fa-2xl mr-5">
-                            <i class="fa-solid fa-circle-user text-red-500 "></i>
-                            <button id="dropdownDefault" data-dropdown-toggle="dropdown" class=" bg-white focus:outline-none font-medium rounded-lg text-sm px-4 py-2.5 text-center inline-flex items-center " type="button"><i class="fa-solid fa-ellipsis-vertical fa-xl hover:text-red-500"></i><button>
-                            <!-- Dropdown menu -->
-                            <div id="dropdown" class="z-10 hidden bg-white divide-y divide-gray-100 rounded shadow w-44 ">
-                                <ul class="py-1 text-sm font-semibold text-black  " aria-labelledby="dropdownDefault">
-                                    @guest
+                            <li>
+                                <a href="{{ route('login') }}" class="block px-4 py-2  hover:text-red-500">Connexion</a>
+                            </li>
 
-                                    <li>
-                                        <a href="{{route('login')}}" class="block px-4 py-2  hover:text-red-500">Connexion</a>
-                                    </li>
+                            <li>
+                                <a href="{{ route('register') }}"
+                                    class="block px-4 py-2  hover:text-red-500">Inscription</a>
+                            </li>
+                        @endguest
+                        <li>
+                            <a href="{{ route('index') }}" class="block px-4 py-2  hover:text-red-500">Accueil</a>
+                        </li>
+                        @auth
 
-                                    <li>
-                                        <a href="{{route('register')}}" class="block px-4 py-2  hover:text-red-500">Inscription</a>
-                                    </li>
-                                    @endguest
-                                  <li>
-                                    <a href="{{route('index')}}" class="block px-4 py-2  hover:text-red-500">Accueil</a>
-                                  </li>
-                                  @auth
+                            <li>
+                                <a href="{{ route('biblio', Auth::user()->id) }}"
+                                    class="block px-4 py-2 hover:text-red-500">Bibliothèque</a>
+                            </li>
 
-                                  <li>
-                                      <a href="{{route('biblio', Auth::user()->id)}}" class="block px-4 py-2 hover:text-red-500">Bibliothèque</a>
-                                    </li>
+                            <li>
+                                <a href="{{ route('user') }}" class="block px-4 py-2 hover:text-red-500">Dashboard</a>
+                            </li>
 
-                                    <li>
-                                        <a href="{{route('user')}}" class="block px-4 py-2 hover:text-red-500">Dashboard</a>
-                                    </li>
+                            <li>
+                                <a href="{{ route('signout') }}" class="block px-4 py-2 hover:text-red-500">Sign Out</a>
+                            </li>
+                        @endauth
+                    </ul>
+                </div>
+    </div>
 
-                                    <li>
-                                        <a href="{{route('signout')}}" class="block px-4 py-2 hover:text-red-500">Sign Out</a>
-                                    </li>
-                                    @endauth
-                                </ul>
-                            </div>
-                            </div>
-                            </div>
-                            <script src="https://unpkg.com/flowbite@1.4.7/dist/flowbite.js"></script>
+    <script src="https://unpkg.com/flowbite@1.4.7/dist/flowbite.js"></script>
 
-              </div>
-         </div>
 
+
+
+    <form class="d-flex   md:order-2" method="GET" action="{{ route('results') }}">
+        <input class=" h-7 rounded-full form-control me-2" type="search" name="search_query" placeholder="Search" aria-label="Search">
+        <button class="btn btn-outline-success" type="submit"> ok </button>
+    </form>
 
 </nav>
